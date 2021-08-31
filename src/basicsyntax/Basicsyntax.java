08fa696e8a76e97049d243f14f88c6d8924f238f
AbdulMoizChishti <moaiz599@gmail.com>
2021-08-31 19:20:53 +0500

    Basic Syntax

---
+++ src/basicsyntax/Basicsyntax.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicsyntax;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class Basicsyntax {
+    public static void main(String args[]){
+    
+    int a=234;
+    String b= "hello world";
+    double c=67.66;
+    System.out.println(b);
+    
+    
+    
+    }
+    
+    
+}
